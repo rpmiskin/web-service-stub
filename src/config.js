b33fe576cc2ddb5064f0a28b79f31e6c64fe4cda
Richard Miskin <r.p.miskin@gmail.com>
2018-03-19 14:24:24 +0000

    Added route for configuring other routes - not quite working!

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  path: process.env.STUB_PATH || "/stub"
 };
--- src/index.js
+++ src/index.js
@@ -1,38 +1,126 @@
 const express = require("express");
-const config = require("./config");
+const serverConfig = require("./config");
+const bodyParser = require("body-parser");
 
-function wrapper(fn) {
-  return (req, res) => {
-    console.log(`wrapper ${JSON.stringify(res.headers)}`);
-    return fn(req, res);
-  };
+let server;
+let requestHistory = {};
+const stubbedPaths = {
+  // Will contain things like:
+  // {
+  //   path:'foo/bar',
+  //   method:'GET',
+  //   type:'json'
+  //   response:[{status:200, headers:{}, body:""}]
+  // }
+};
+
+const app = express();
+app.use(bodyParser.json());
+
+function checkConfig(config) {
+  if (!config) {
+    return { error: "No config specified" };
+  }
+  if (!config.path) {
+    return { error: "No path specified" };
+  }
+
+  if (!config.method) {
+    return { error: "No method specified" };
+  }
+  if (!config.response) {
+    return { error: "No response specified" };
+  }
+
+  // Have we already configured this path?
+  if (stubbedPaths[config.path] && stubbedPaths[config.path][config.method]) {
+    return {
+      error: `Path ${config.method} ${config.path} has already be configured.`
+    };
+  }
+  return undefined;
 }
 
-let requests = [];
-const realApp = express();
-const app = new Proxy(realApp, {
-  get: (target, path, fn) => target.get(path, wrapper(fn))
-});
-
-app.get("/", (req, res) => {
-  // console.log(JSON.stringify(req.headers, null, 3));
-  // console.log(JSON.stringify(req.path, null, 3));
-  requests.push({
-    path: req.path,
-    headers: req.headers
+function configureStub(config) {
+  if (!stubbedPaths[config.path]) {
+    stubbedPaths[config.path] = {};
+  }
+  stubbedPaths[config.path][config.method] = config.response;
+  let router;
+  switch (config.method) {
+    case "GET":
+      router = app.get;
+      break;
+    case "POST":
+      router = app.post;
+      break;
+    default:
+      console.log(`Unexpected method ${config.method}, defaulting to GET`);
+      router = app.get;
+  }
+  app.get(config.path, (req, res) => {
+    console.log("aaa");
+    // Match path to stubbedPaths.
+    if (!stubbedPaths[config.path]) {
+      return res.status(404).send();
+    }
+    // Check for responses.
+    let stubResponse = stubbedPaths[config.path].response;
+    if (Array.isArray(stubResponse)) {
+      if (Array.isArray(stubResponse.length === 0)) {
+        console.log(`responses for ${config.method} ${config.path} exhausted`);
+        return res.status(404).send();
+      }
+      stubResponse = stubResponse.unshift();
+    }
+    // Use the response.
+    if (stubResponse.status) {
+      res.status(stubResponse.status);
+    }
+    if (stubResponse.headers) {
+      Object.keys(stubResponse.headers).forEach(k =>
+        res.set(k, stubResponse.headers[k])
+      );
+    }
+    if (stubResponse.body) {
+      return res.send(stubResponse.body);
+    }
+    return res.end();
   });
-  return res.send("Hello World!");
-});
+}
 
-app.get("/stub", (req, res) => {
+app.get(serverConfig.path, (req, res) => {
   console.log("returning requests...");
-  return res.json(requests);
+  return res.json(requestHistory);
 });
-
-app.delete("/stub", (req, res) => {
-  console.log("clearing requests...");
-  requests = [];
+app.delete(serverConfig.path, (req, res) => {
+  console.log("clearing configuration and request history...");
+  requestHistory = [];
   return res.send("Cleared!");
 });
 
-app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
+app.post(serverConfig.path, (req, res) => {
+  console.log("configuring new path...");
+  console.log(JSON.stringify(req.body));
+  const validationError = checkConfig(req.body);
+  if (validationError) {
+    return res.status(403).json(validationError);
+  }
+  try {
+    res.status(200).json(configureStub(req.body));
+    server.close();
+    setImmediate(() => {
+      server.emit("close");
+    });
+    server = app.listen(serverConfig.port);
+  } catch (e) {
+    console.log(e.message);
+    console.log(e.stack);
+    return res.status(503).end();
+  }
+});
+
+server = app.listen(serverConfig.port, () =>
+  console.log(`Listening on port ${serverConfig.port}`)
+);
+server.on("connection", () => console.log("foo!"));
