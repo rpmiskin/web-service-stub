114f33f7cc8acb43b61ece2eff6630ffeb5585fd
Richard Miskin <r.p.miskin@gmail.com>
2018-03-19 21:03:34 +0000

    Works for initial proof of concept. Added README.md

---
+++ README.md
@@ -0,0 +1,102 @@
+# web-service-stub
+
+Recently I've been working on system testing some [node.js](http://nodejs.org) microservices, by spinning them up in a Swarm of [Docker](https://www.docker.com) containers and then driving them from a [Jest](https://facebook.github.io/jest) test script.
+
+I've been using Canned to stub other systems that are called but I've come across two problems:
+
+1.  Test config is spread between my test scripts and my canned configuration.
+2.  I cannot make assertions on the calls that have been made to Canned.
+
+To that end, I've created web-service-stub which allows new endpoints to be spun up, with defined behaviours **and** records all of the calls that have been made for later assertion.
+
+## Setup
+
+By default the service starts up on port 3000 and can be called on path `/stub` e.g. http://localhost:3000/stub
+
+To change the port use the `PORT` environment variable and to change the path use the `STUB_PATH` environment variable. For example:
+
+    PORT=1234 STUB_PATH=/not/stub/path npm start
+
+## Configuring endpoints
+
+POST JSON configuration to http://localhost:3000/stub
+
+The following JSON will set up the /bar endpoint to receive GET calls. All calls to GET with receive the specified response.
+
+    {
+      "path": "/bar",
+      "method": "GET",
+      "response":
+        {
+          "status": 200,
+          "type": "json",
+          "headers": {
+            "header1": "value1",
+            "header2": "value2"
+          },
+          "body": "{\"a\":\"b\"}"
+        }
+      ]
+    }
+
+In the following example the response element has been defined as an array. In this case the first call will get the response[0], then response[1] etc until the array is empty at which point empty 404 responses will be returned
+
+    {
+      "path": "/bar",
+      "method": "GET",
+      "response": [
+        {
+          "status": 200,
+          "type": "json",
+          "headers": {
+            "header1": "value1",
+            "header2": "value2"
+          },
+          "body": "{\"a\":\"b\"}"
+        },
+        {
+          "status": 500
+        }
+      ]
+    }
+
+Attempting to configure an endpoint that has already been configured (and not clea)
+
+## Checking calls
+
+Use HTTP GET to http://localhost:3000/stub
+
+This will return a JSON object as follows:
+
+    {
+      "/foo": {
+        "GET": [
+          {
+            "body": {},
+            "headers": {
+              "host": "localhost:3000",
+              "user-agent": "curl/7.54.0",
+              "accept": "*/*"
+            },
+            "query": { "re": "" },
+            "params": {}
+          },
+          {
+            "body": {},
+            "headers": {
+              "host": "localhost:3000",
+              "user-agent": "curl/7.54.0",
+              "accept": "*/*"
+            },
+            "query": { "re": "" },
+            "params": {}
+          }
+        ]
+      }
+    }
+
+Each path and HTTP method that has been invoked has an array of objects showing the request body, headers, query and params. The order of elements in the array reflects the order of the calls.
+
+## Clearing configuration
+
+Use a HTTP DELETE http://localhost:3000/stub
--- src/index.js
+++ src/index.js
@@ -1,21 +1,52 @@
 const express = require("express");
 const serverConfig = require("./config");
 const bodyParser = require("body-parser");
+const killable = require("killable");
 
 let server;
-let requestHistory = {};
-const stubbedPaths = {
-  // Will contain things like:
+
+let requestHistory = {
+  // Will contain things like
   // {
-  //   path:'foo/bar',
-  //   method:'GET',
-  //   type:'json'
-  //   response:[{status:200, headers:{}, body:""}]
+  //   "/foo": {
+  //     "GET": [
+  //       {
+  //         "body": {},
+  //         "headers": {
+  //           "host": "localhost:3000",
+  //           "user-agent": "curl/7.54.0",
+  //           "accept": "*/*"
+  //         },
+  //         "query": { "re": "" },
+  //         "params": {}
+  //       },
+  //       {
+  //         "body": {},
+  //         "headers": {
+  //           "host": "localhost:3000",
+  //           "user-agent": "curl/7.54.0",
+  //           "accept": "*/*"
+  //         },
+  //         "query": { "re": "" },
+  //         "params": {}
+  //       }
+  //     ]
+  //   }
   // }
 };
-
-const app = express();
-app.use(bodyParser.json());
+let stubbedPaths = {
+  // // Will contain things like:
+  // "/foo/bar": {
+  //   GET: {
+  //     type: "json",
+  //     response: [{ status: 200, headers: {}, body: "" }]
+  //   },
+  //   DELETE: {
+  //     type: "json",
+  //     response: { status: 503, headers: {}, body: "" }
+  //   }
+  // }
+};
 
 function checkConfig(config) {
   if (!config) {
@@ -24,7 +55,6 @@ function checkConfig(config) {
   if (!config.path) {
     return { error: "No path specified" };
   }
-
   if (!config.method) {
     return { error: "No method specified" };
   }
@@ -35,10 +65,61 @@ function checkConfig(config) {
   // Have we already configured this path?
   if (stubbedPaths[config.path] && stubbedPaths[config.path][config.method]) {
     return {
-      error: `Path ${config.method} ${config.path} has already be configured.`
+      error: `Path ${config.method} ${config.path} has already been configured.`
     };
   }
   return undefined;
+}
+
+function save(args) {
+  const { path, method, req } = args;
+  if (!requestHistory[path]) {
+    requestHistory[path] = {};
+  }
+  if (!requestHistory[path][method]) {
+    requestHistory[path][method] = [];
+  }
+  requestHistory[path][method].push({
+    body: req.body,
+    headers: req.headers,
+    query: req.query,
+    params: req.params
+  });
+}
+function getResponse(args) {
+  const { path, method } = args;
+  // Check for responses.
+  let stubResponse = stubbedPaths[path][method];
+  if (Array.isArray(stubResponse)) {
+    if (stubResponse.length === 0) {
+      console.log(`responses for ${method} ${path} exhausted`);
+      return { status: 404 };
+    }
+    stubResponse = stubResponse.shift();
+  }
+  return stubResponse;
+}
+function send(args) {
+  const { res } = args;
+  const stubResponse = getResponse(args);
+  // Use the response.
+  if (stubResponse.status) {
+    res.status(stubResponse.status);
+  }
+  if (stubResponse.headers) {
+    Object.keys(stubResponse.headers).forEach(k =>
+      res.set(k, stubResponse.headers[k])
+    );
+  }
+  if (stubResponse.body) {
+    return res.send(stubResponse.body);
+  }
+  return res.end();
+}
+
+function handler(args) {
+  save(args);
+  send(args);
 }
 
 function configureStub(config) {
@@ -46,81 +127,72 @@ function configureStub(config) {
     stubbedPaths[config.path] = {};
   }
   stubbedPaths[config.path][config.method] = config.response;
-  let router;
-  switch (config.method) {
-    case "GET":
-      router = app.get;
-      break;
-    case "POST":
-      router = app.post;
-      break;
-    default:
-      console.log(`Unexpected method ${config.method}, defaulting to GET`);
-      router = app.get;
-  }
-  app.get(config.path, (req, res) => {
-    console.log("aaa");
-    // Match path to stubbedPaths.
-    if (!stubbedPaths[config.path]) {
-      return res.status(404).send();
+}
+
+function startServer() {
+  const app = express();
+  app.use(bodyParser.json());
+
+  app.get(serverConfig.path, (req, res) => {
+    console.log("returning requests...");
+    return res.json(requestHistory);
+  });
+
+  app.delete(serverConfig.path, (req, res) => {
+    console.log("clearing configuration and request history...");
+    requestHistory = [];
+    stubbedPaths = {};
+    res.send("Cleared!");
+    server.kill(() => {
+      console.log("Restarting server...");
+      startServer();
+    });
+  });
+
+  app.post(serverConfig.path, (req, res) => {
+    console.log("configuring new path...");
+    const validationError = checkConfig(req.body);
+    if (validationError) {
+      return res.status(403).json(validationError);
     }
-    // Check for responses.
-    let stubResponse = stubbedPaths[config.path].response;
-    if (Array.isArray(stubResponse)) {
-      if (Array.isArray(stubResponse.length === 0)) {
-        console.log(`responses for ${config.method} ${config.path} exhausted`);
-        return res.status(404).send();
+    try {
+      res.status(200).json(configureStub(req.body));
+      return server.kill(() => {
+        console.log("Restarting server...");
+        startServer();
+      });
+    } catch (e) {
+      console.log(e.message);
+      console.log(e.stack);
+      return res.status(503).end();
+    }
+  });
+
+  // Iterate through all of the stubbed paths, creating the handlers.
+  Object.keys(stubbedPaths).forEach(path =>
+    Object.keys(stubbedPaths[path]).forEach(method => {
+      switch (method) {
+        case "GET":
+          app.get(path, (req, res) => handler({ req, res, path, method }));
+          break;
+        case "POST":
+          app.post(path, (req, res) => handler({ req, res, path, method }));
+          break;
+        case "PUT":
+          app.put(path, (req, res) => handler({ req, res, path, method }));
+          break;
+
+        default:
+          console.log(`Unsupported method ${method} ${path}`);
+          break;
       }
-      stubResponse = stubResponse.unshift();
-    }
-    // Use the response.
-    if (stubResponse.status) {
-      res.status(stubResponse.status);
-    }
-    if (stubResponse.headers) {
-      Object.keys(stubResponse.headers).forEach(k =>
-        res.set(k, stubResponse.headers[k])
-      );
-    }
-    if (stubResponse.body) {
-      return res.send(stubResponse.body);
-    }
-    return res.end();
+    })
+  );
+
+  server = app.listen(serverConfig.port, () => {
+    killable(server);
+    console.log(`Listening on port ${serverConfig.port}`);
   });
 }
 
-app.get(serverConfig.path, (req, res) => {
-  console.log("returning requests...");
-  return res.json(requestHistory);
-});
-app.delete(serverConfig.path, (req, res) => {
-  console.log("clearing configuration and request history...");
-  requestHistory = [];
-  return res.send("Cleared!");
-});
-
-app.post(serverConfig.path, (req, res) => {
-  console.log("configuring new path...");
-  console.log(JSON.stringify(req.body));
-  const validationError = checkConfig(req.body);
-  if (validationError) {
-    return res.status(403).json(validationError);
-  }
-  try {
-    res.status(200).json(configureStub(req.body));
-    server.close();
-    setImmediate(() => {
-      server.emit("close");
-    });
-    server = app.listen(serverConfig.port);
-  } catch (e) {
-    console.log(e.message);
-    console.log(e.stack);
-    return res.status(503).end();
-  }
-});
-
-server = app.listen(serverConfig.port, () =>
-  console.log(`Listening on port ${serverConfig.port}`)
-);
-server.on("connection", () => console.log("foo!"));
+startServer();
